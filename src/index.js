b24e2f9a2a30cf767f10b610e5d1439ab290fa26
Gabriel Rodriguez <gabrielsro182@gmail.com>
2023-02-24 08:19:15 -0500

    tracking down promise

--- src/index.js
+++ src/index.js
@@ -6,10 +6,15 @@ makeSearchBar();
 getOptions(false);
 
 if (localStorage.length > 0) {
+  console.log("something");
+  console.log(`${localStorage.length}`);
+  console.log(JSON.parse(localStorage.getItem("cities")));
   if (JSON.parse(localStorage.getItem("cities")).length > 0) {
     favoritesBar.makeFavoritesBar();
   }
 } else {
+  console.log("nothing");
+  console.log(`${localStorage.length}`);
   let citiesArray = [];
   let citiesArrayJSON = JSON.stringify(citiesArray);
   localStorage.setItem("cities", `${citiesArrayJSON}`);
